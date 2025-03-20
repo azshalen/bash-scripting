981e6dc68757f86413ca6f26234796e47289df94
azshalen <viktor.donchev.en@gmail.com>
2025-03-20 19:23:28 +0100

    Completed with AWK.

--- basics/awk.sh
+++ basics/awk.sh
@@ -19,7 +19,21 @@
 #	NF		- number of fields
 #	-f file		- specify a file that contains an awk script
 
-cat ~/bash-scripting/text-files/passwd.txt | awk -F ":" '{print $1,$3,$5}'
+#awk -F ":" '{print $1,$3,$5}' /etc/passwd
 
+# Will print whole line, $0 means current line.
+#awk '{print $0}' /etc/passwd
 
+# Will print whole lines starting from line number 5.
+#awk 'NR >= 5 {print $0}' /etc/passwd
 
+# AWK BEGIN & END
+# awk 'BEGIN {start_action} PATTERN/CONDITION {action} END {stop_action}' file_name
+# awk -f awk.script file_name
+
+# BEGIN	- block is performed before reading the file
+# END	- block is performed after processing the file
+
+awk 'BEGIN {print "-"} /nologin/ {print $0} END {print "-"}' /etc/passwd
+
+#awk '{a=$1; b=$2; print a+b}' file_name.txt (file_name.txt contains 2 6)
